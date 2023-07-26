f04a668a58e6b44f0e0c10dee50be06189d572c6
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 20:26:15 -0500

    Weird ghost apostrophe

--- Examples/KMeansSegment/octave_pkg_setup.m
+++ Examples/KMeansSegment/octave_pkg_setup.m
@@ -1,12 +1,12 @@
 if(0 == length(pkg('list', 'statistics')))
     printf("\tInstalling statistics");
-    pkg('install' 'https://github.com/gnu-octave/statistics/archive/refs/tags/release-1.5.0.tar.gz');
+    pkg('install', 'https://github.com/gnu-octave/statistics/archive/refs/tags/release-1.5.0.tar.gz');
 end
-pkg('load' 'statistics');
+pkg('load', 'statistics');
 
 if(0 == length(pkg('list', 'image')))
     printf("\tInstalling image");
-    pkg('install' '-forge' 'image');
+    pkg('install', '-forge', 'image');
 end
-pkg('load' 'image');
+pkg('load', 'image');
 
