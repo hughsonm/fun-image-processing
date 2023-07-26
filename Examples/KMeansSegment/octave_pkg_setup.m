e9450738ce8dc6dd8e88029f799f67eb4cc4a673
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 20:21:22 -0500

    Install lower-level statistics

--- Examples/KMeansSegment/octave_pkg_setup.m
+++ Examples/KMeansSegment/octave_pkg_setup.m
@@ -1,13 +1,12 @@
-required_pkgs = {'image', 'statistics'};
-
-for ii = 1:length(required_pkgs)
-    req_pkg = required_pkgs{ii};
-    printf("Checking for package named %s\n", req_pkg);
-    if(0 == length(pkg('list', req_pkg)))
-        printf("\tIt does not exist. Installing %s\n", req_pkg);
-        pkg('install', '-forge', req_pkg)
-        printf("\tDone installing %s\n", req_pkg);
-    end
-    printf("Loading %s\n", req_pkg);
-    pkg('load', req_pkg)
+if(0 == length(pkg('list', 'statistics')))
+    printf("\tInstalling statistics");
+    pkg('install' 'https://github.com/gnu-octave/statistics/archive/refs/tags/release-1.5.0.tar.gz');
 end
+pkg('load' 'statistics');
+
+if(0 == length(pkg('list', 'image')))
+    printf("\tInstalling image");
+    pkg('install' '-forge' 'image');
+end
+pkg('load' 'image');
+
