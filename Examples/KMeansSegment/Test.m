4e063be76a09c0837b64b40dfb6819c79f7ae08d
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 20:36:56 -0500

    Do the setup in the test function

--- Examples/KMeansSegment/Test.m
+++ Examples/KMeansSegment/Test.m
@@ -7,6 +7,20 @@ NUM_SEGMENTS = 16;
 top_dir = fullfile("..","..");
 addpath(top_dir);
 
+if exist('OCTAVE_VERSION', 'builtin')
+    if(0 == length(pkg('list', 'statistics')))
+        printf("\tInstalling statistics");
+        pkg('install', 'https://github.com/gnu-octave/statistics/archive/refs/tags/release-1.5.0.tar.gz');
+    end
+    pkg('load', 'statistics');
+
+    if(0 == length(pkg('list', 'image')))
+        printf("\tInstalling image");
+        pkg('install', '-forge', 'image');
+    end
+    pkg('load', 'image');
+end
+
 % Image Credit:
 % "Monkey Face" by MarkGrundland is licensed under CC BY 2.0
 
