7b834f25426500189f7e33f8c24634de49221266
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 19:51:17 -0500

    Prep for Octave

--- Examples/KMeansSegment/Test.m
+++ Examples/KMeansSegment/Test.m
@@ -1,8 +1,8 @@
 close all;
 clearvars;
 
-IMAGE_SCALE = 1.0;
-NUM_SEGMENTS = 64;
+IMAGE_SCALE = 0.5;
+NUM_SEGMENTS = 16;
 
 top_dir = fullfile("..","..");
 addpath(top_dir);
@@ -10,14 +10,12 @@ addpath(top_dir);
 % Image Credit:
 % "Monkey Face" by MarkGrundland is licensed under CC BY 2.0
 
-
-
-I = imread("~/Downloads/Kira-Thigh.jpg");
+I = imread("MonkeyFace.jpg");
 R = imresize(I,IMAGE_SCALE);
 K = KMeansSegment(R,NUM_SEGMENTS);
 
 ff = figure("Name",sprintf("K-Means with %d segments, scaled %.3fx", NUM_SEGMENTS, IMAGE_SCALE));
 imshow(K);
 
-output_name = sprintf("Kira-Thigh-%d-%.3f.png", NUM_SEGMENTS, IMAGE_SCALE);
-imwrite(K,fullfile("~", "Pictures","Kira", output_name));
+output_name = sprintf("MonkeyFace-%d-%.3f.png", NUM_SEGMENTS, IMAGE_SCALE);
+imwrite(K, output_name);
---
+++ Examples/KMeansSegment/octave_pkg_setup.m
@@ -0,0 +1,13 @@
+required_pkgs = {'image', 'statistics'};
+
+for ii = 1:length(required_pkgs)
+    req_pkg = required_pkgs{ii};
+    printf("Checking for package named %s\n", req_pkg);
+    if(0 == length(pkg('list', req_pkg)))
+        printf("\tIt does not exist. Installing %s\n", req_pkg);
+        pkg('install', req_pkg)
+        printf("\tDone installing %s\n", req_pkg);
+    end
+    printf("Loading %s\n", req_pkg);
+    pkg('load', req_pkg)
+end
