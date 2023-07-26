90d0198ed3a5d96d04327e758ff5b4ca12ee79b7
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 19:58:00 -0500

    Fix pkg syntax

--- Examples/KMeansSegment/octave_pkg_setup.m
+++ Examples/KMeansSegment/octave_pkg_setup.m
@@ -5,7 +5,7 @@ for ii = 1:length(required_pkgs)
     printf("Checking for package named %s\n", req_pkg);
     if(0 == length(pkg('list', req_pkg)))
         printf("\tIt does not exist. Installing %s\n", req_pkg);
-        pkg('install', req_pkg)
+        pkg('install', '-forge', req_pkg)
         printf("\tDone installing %s\n", req_pkg);
     end
     printf("Loading %s\n", req_pkg);
