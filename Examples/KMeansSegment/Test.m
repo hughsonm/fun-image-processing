c75a8165d74eeb6765bee415956c25aa3a92ae9a
Max Hughson <hughsonm@myumanitoba.ca>
2023-07-25 19:38:09 -0500

    Update Example.m for Octave

--- Examples/KMeansSegment/Test.m
+++ Examples/KMeansSegment/Test.m
@@ -2,24 +2,22 @@ close all;
 clearvars;
 
 IMAGE_SCALE = 1.0;
-NUM_SEGMENTS = 16;
+NUM_SEGMENTS = 64;
 
 top_dir = fullfile("..","..");
 addpath(top_dir);
 
 % Image Credit:
-% "Monkey Face" by MarkGrundland is licensed under CC BY 2.0 
+% "Monkey Face" by MarkGrundland is licensed under CC BY 2.0
 
 
 
-I = imread("MonkeyFace.jpg");
+I = imread("~/Downloads/Kira-Thigh.jpg");
 R = imresize(I,IMAGE_SCALE);
 K = KMeansSegment(R,NUM_SEGMENTS);
 
-ff = figure();
+ff = figure("Name",sprintf("K-Means with %d segments, scaled %.3fx", NUM_SEGMENTS, IMAGE_SCALE));
 imshow(K);
-ff.Name = "K-Means with " + NUM_SEGMENTS +...
-    " segments, imaged scaled " + IMAGE_SCALE + "x";
 
-output_name = "MonkeyFace-" + NUM_SEGMENTS + ".jpg";
-imwrite(K,output_name);
+output_name = sprintf("Kira-Thigh-%d-%.3f.png", NUM_SEGMENTS, IMAGE_SCALE);
+imwrite(K,fullfile("~", "Pictures","Kira", output_name));
